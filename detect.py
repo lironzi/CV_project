2af0671c27f2e1636448ff4c58b1195544b5c862
lironzi <74775238+lironzi@users.noreply.github.com>
2021-01-09 23:54:43 +0200

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,150 @@
+import argparse
+import time
+from pathlib import Path
+
+import os
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
+    strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
+
+def detect(save_img=False,weights='yolov5s.pt', source='data/images',imgsz=512,conf_thres=0.25,iou_thres=0.45,myAnnFileName):
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(('rtsp://', 'rtmp://', 'http://'))
+    
+    #default_parameters:
+    exist_ok=False
+    project='runs/detect'
+    name='exp'
+    save_txt=False
+    view_img=False
+    vid_path, vid_writer = None, None
+    
+    save_dir=Path(increment_path(Path(project) / name, exist_ok))
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)
+    
+    # Initialize
+    set_logging()
+    device = select_device('')
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+    if half:
+        model.half()  # to FP16
+
+
+    dataset = LoadImages(source, img_size=imgsz)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    
+    
+    #
+    
+    script_dir = os.path.dirname(__file__)
+    rel_path = myAnnFileName
+    abs_file_path = os.path.join(script_dir, rel_path)
+    file_annot=open(abs_file_path,'w')
+    #
+    
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+    for path, img, im0s, vid_cap in dataset:
+        
+        
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        pred = model(img, False)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, None, False)
+        t2 = time_synchronized()
+
+
+        
+        #
+        p, s, im0 = Path(path), '', im0s
+        file_annot.write(p.stem.split('_')[0]+'.JPG:')
+        xyxy_2print=[]
+        #
+        
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0 = Path(path[i]), '%g: ' % i, im0s[i].copy()
+            else:
+                p, s, im0 = Path(path), '', im0s
+
+            save_path = str(save_dir / p.name)
+            txt_path = str(save_dir / 'labels' / p.stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
+            
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    xyxy_2print.append([int(elem.item()) for elem in xyxy])
+                    xyxy_2print[-1].append(int(cls.item()))
+
+            # Print time (inference + NMS)
+            print('%sDone. (%.3fs)' % (s, t2 - t1))
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str(p), im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'images':
+                    cv2.imwrite(save_path, im0)
+                else:
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+
+                        fourcc = 'mp4v'  # output video codec
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+                    vid_writer.write(im0)
+        xyxy_2print=str(xyxy_2print)
+        xyxy_2print=xyxy_2print[1:-1]
+        file_annot.write(xyxy_2print)
+        file_annot.write('\n')
+
+    file_annot.close()
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
---
+++ runMe.py
@@ -0,0 +1,56 @@
+import numpy as np
+import ast
+import os
+import detect
+
+
+def run(myAnnFileName, buses):
+    
+    #if weights file in the project folder
+    script_dir = os.path.dirname(__file__)
+    rel_path = 'best_yolo5_2.pt'
+    weights_path = os.path.join(script_dir, rel_path)
+    #if weights file in drive
+    
+    rel_path=buses
+    detect_path=os.path.join(script_dir, rel_path)
+    
+
+    #detect(save_img=False,weights='yolov5s.pt', source='data/images',imgsz=512,conf_thres=0.25,iou_thres=0.45)
+    
+    detect.detect(False,weights_path,detect_path,512,0.25,0.45,myAnnFileName)
+
+
+    
+    
+    
+    # print ('Running dummy runMe')
+    # annFileNameGT = os.path.join(os.getcwd(),'annotationsTrain.txt')
+    # writtenAnnsLines = {}
+    # annFileEstimations = open(myAnnFileName, 'w+')
+    # annFileGT = open(annFileNameGT, 'r')
+    # writtenAnnsLines['Ground_Truth'] = (annFileGT.readlines())
+
+    # for k, line_ in enumerate(writtenAnnsLines['Ground_Truth']):
+
+    #     line = line_.replace(' ','')
+    #     imName = line.split(':')[0]
+    #     anns_ = line[line.index(':') + 1:].replace('\n', '')
+    #     anns = ast.literal_eval(anns_)
+    #     if (not isinstance(anns, tuple)):
+    #         anns = [anns]
+    #     corruptAnn = [np.round(np.array(x) + np.random.randint(low = 0, high = 100, size = 5)) for x in anns]
+    #     corruptAnn = [x[:4].tolist() + [anns[i][4]] for i,x in enumerate(corruptAnn)]
+    #     strToWrite = imName + ':'
+    #     if(3 <= k <= 5):
+    #         strToWrite += '\n'
+    #     else:
+    #         for i, ann in enumerate(corruptAnn):
+    #             posStr = [str(x) for x in ann]
+    #             posStr = ','.join(posStr)
+    #             strToWrite += '[' + posStr + ']'
+    #             if (i == int(len(anns)) - 1):
+    #                 strToWrite += '\n'
+    #             else:
+    #                 strToWrite += ','
+    #     annFileEstimations.write(strToWrite)
